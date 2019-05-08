7e01520e38552a8abf342616a0024b16f61dd207
youyuan.lin <youyuan.lin@tongdun.cn>
2019-05-08 19:44:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# react脚手架
+
+## 前言
+
+每次使用官方的`create-react-app`脚手架，都需要配置一堆东西。这里在`create-react-app`的基础上增加功能，不进行`eject`保留`react-cript`，目的是保证和官方一致并且可以升级，把`eject`的权利保留给用户。所以这个脚手架是`create-react-app`的拓展，主要增加的框架扩展如下。
+
+类型 | 可选框架名称
+:- | :- 
+语言 | JavaScript / TypeScript
+状态管理库 | Redux / Mobx
+css预处理器 | SCSS / LESS / Styled-Components
+UI组件 | Antd / Ant-mobile
+代码规范 | Airbnb
+
+### 使用命令 
+
+`react-cli create <app-name> `
+
+选择`default (JavaScript, Redux, Antd, Less, Router, ESLint)` 或者 `Manually select features `进行选择配置。
---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const program = require('commander');
+const chalk = require("chalk");
+
+program
+  .version(require('./package').version)
+  .usage('<command> [options]');
+
+program
+  .command('create <app-name>')
+  .description('create a new project powered by react-cli')
+  .action(name => {
+    const create = require('./lib/cli/create');
+    const { clearConsole } = require('./lib/utils');
+    clearConsole(chalk.blue(`React CLI v${require('./package').version}`));
+    create(name);
+  });
+
+program
+  .arguments('<command>')
+  .action((cmd) => {
+    program.outputHelp()
+    console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+    console.log()
+  })
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.outputHelp();
+}
